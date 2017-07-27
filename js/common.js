5dbe8d88d8378cb2a479f74313aa96af9cff0320
Denys Vladymyrov <link_amd@yahoo.com>
2017-07-27 21:52:53 +0300

    added 'Choose Design' panel

--- js/common.js
+++ js/common.js
@@ -1,105 +1,129 @@
 
 
-var topLine = $(".top-line");
-var mainContent = $("main");
-var offSet = $(".top-line-main").offset();
-var isFixed = false;
-var topLineFees = $(".top-line-fees");
+// Sticky Header
+(function () {
+  var topLine = $(".top-line");
+  var mainContent = $("main");
+  var offSet = $(".top-line-main").offset();
+  var isFixed = false;
+  var topLineFees = $(".top-line-fees");
 
-$(window).scroll(function() {
+  $(window).scroll(function() {
 
-  if( $(this).scrollTop() > offSet.top && offSet.top != 0) {
-    topLine.addClass("top-line-fixed");
+    if( $(this).scrollTop() > offSet.top && offSet.top != 0) {
+      topLine.addClass("top-line-fixed");
 
-    if(!isFixed) {
-      topLineFees.show();
-      var offsetTop = parseInt(mainContent.css("margin-top"));
-      offsetTop += parseInt(topLine.outerHeight());
-      mainContent.css("margin-top", offsetTop);
-      isFixed = true;
-    }
+      if(!isFixed) {
+        topLineFees.show();
+        var offsetTop = parseInt(mainContent.css("margin-top"));
+        offsetTop += parseInt(topLine.outerHeight());
+        mainContent.css("margin-top", offsetTop);
+        isFixed = true;
+      }
 
-  } else if(offSet.top != 0){
+    } else if(offSet.top != 0){
 
-    if(isFixed) {
-      topLineFees.hide();
-      var offsetTop = parseInt(mainContent.css("margin-top")) - topLineFees.outerHeight();
-      offsetTop -= parseInt(topLine.outerHeight());
-      mainContent.css("margin-top", offsetTop);
-    }
+      if(isFixed) {
+        topLineFees.hide();
+        var offsetTop = parseInt(mainContent.css("margin-top")) - topLineFees.outerHeight();
+        offsetTop -= parseInt(topLine.outerHeight());
+        mainContent.css("margin-top", offsetTop);
+      }
 
-    topLine.removeClass("top-line-fixed");
-    isFixed = false;
-  }
-});
-
-
-function moveProgressBar(animationDelay = 0) {
-  var getPercent = ($('.progress-wrap').data('progress-percent') / 100);
-  var getProgressWrapWidth = $('.progress-wrap').width();
-  var progressTotal = getPercent * getProgressWrapWidth;
-  var animationLength = 100;
-
-  $('.progress-bar').stop().delay(animationDelay).animate({
-    left: progressTotal
-  }, animationLength);
-}
-
-
-function countActiveNavLi(){
-  var activeNavLi = 0;
-
-  $(".nav-list-progress li").each(function( index ) {
-    if($(this).hasClass("active")){
-      activeNavLi++;
+      topLine.removeClass("top-line-fixed");
+      isFixed = false;
     }
   });
-
-  return activeNavLi;
-}
+}());
 
 
-function setProgressBarPercent(animationDelay = 0){
-  var progress  = $('.progress-wrap');
 
-  switch (countActiveNavLi()){
-    case 1: progress.data('progress-percent', 0);
-      break;
-    case 2: progress.data('progress-percent', 27);
-      break;
-    case 3: progress.data('progress-percent', 50);
-      break;
-    case 4: progress.data('progress-percent', 73);
-      break;
-    case 5: progress.data('progress-percent', 100);
-      break;
+// Progress Bar
+(function () {
+
+  function moveProgressBar(animationDelay = 0) {
+    var getPercent = ($('.progress-wrap').data('progress-percent') / 100);
+    var getProgressWrapWidth = $('.progress-wrap').width();
+    var progressTotal = getPercent * getProgressWrapWidth;
+    var animationLength = 100;
+
+    $('.progress-bar').stop().delay(animationDelay).animate({
+      left: progressTotal
+    }, animationLength);
   }
 
-  moveProgressBar(animationDelay);
 
-}
+  function countActiveNavLi(){
+    var activeNavLi = 0;
 
-setProgressBarPercent();
+    $(".nav-list-progress li").each(function( index ) {
+      if($(this).hasClass("active")){
+        activeNavLi++;
+      }
+    });
 
-$(".nav-list-progress li a").on("mouseenter", function(){
-  var progress  = $('.progress-wrap');
-
-  switch ($(this).parent('li').index()){
-    case 0: progress.data('progress-percent', 0);
-      break;
-    case 1: progress.data('progress-percent', 27);
-      break;
-    case 2: progress.data('progress-percent', 50);
-      break;
-    case 3: progress.data('progress-percent', 73);
-      break;
-    case 4: progress.data('progress-percent', 100);
-      break;
+    return activeNavLi;
   }
-  moveProgressBar();
+
+
+  function setProgressBarPercent(animationDelay = 0){
+    var progress  = $('.progress-wrap');
+
+    switch (countActiveNavLi()){
+      case 1: progress.data('progress-percent', 0);
+        break;
+      case 2: progress.data('progress-percent', 27);
+        break;
+      case 3: progress.data('progress-percent', 50);
+        break;
+      case 4: progress.data('progress-percent', 73);
+        break;
+      case 5: progress.data('progress-percent', 100);
+        break;
+    }
+
+    moveProgressBar(animationDelay);
+
+  }
+
+  setProgressBarPercent();
+
+  $(".nav-list-progress li a").on("mouseenter", function(){
+    var progress  = $('.progress-wrap');
+
+    switch ($(this).parent('li').index()){
+      case 0: progress.data('progress-percent', 0);
+        break;
+      case 1: progress.data('progress-percent', 27);
+        break;
+      case 2: progress.data('progress-percent', 50);
+        break;
+      case 3: progress.data('progress-percent', 73);
+        break;
+      case 4: progress.data('progress-percent', 100);
+        break;
+    }
+    moveProgressBar();
+  });
+
+  $(".top-nav").on("mouseleave", function(){
+    setProgressBarPercent(700);
+  });
+
+}());
+
+
+
+
+
+//Events for buttons
+
+$(".customize-btn").on("click", function () {
+  $(".design-header").show();
+  $(".main-header").hide();
 });
 
-$(".top-nav").on("mouseleave", function(){
-   setProgressBarPercent(700);
+$(".discard-btn").on("click", function () {
+  $(".design-header").hide();
+  $(".main-header").show();
 });
-
